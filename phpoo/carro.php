8507bf038cd7c1b10fb8a7e14c1c9ab8532fbb2e
Roney Vila <129080415+akirar0n@users.noreply.github.com>
2024-09-16 17:11:25 -0300

    carro.php

--- phpoo/carro.php
+++ phpoo/carro.php
@@ -5,9 +5,16 @@ require_once 'classes/Carro.class.php';
     $carro1 = new Carro(150);
 
     $carro1->ligar();
+    //$carro1->desligar();
 
-    $carro1->acelerar(30);
+    $carro1->acelerar(20);
+    $carro1->acelerar(50);
+    $carro1->acelerar(50);
+    $carro1->acelerar(30);  
+
     echo "<pre>";
     print_r($carro1);
 
+    echo "<hr>";
+    $carro1->detalhes();
 ?>
